f6b1580d70b3f812eab3255f1c348a6cc7b99da4
Neil-A-C <110299661+Neil-A-C@users.noreply.github.com>
2023-03-22 11:36:19 +0000

    Starting real-time development

--- EnvelopeFollower.cpp
+++ EnvelopeFollower.cpp
@@ -13,6 +13,27 @@ EnvelopeFollower::EnvelopeFollower(int fs, int fc = 10){
     filter.setup(fs, fc);
 }
 
+EnvelopeFollower::~EnvelopeFollower(){
+    stop();
+}
+
+void EnvelopeFollower::start(){
+    follower = std::thread(&EnvelopeFollower::Process, this)
+}
+
+void EnvelopeFollower::stop(){
+    if (follower.joinable()){
+        {
+            std::unique_lock<std::mutex> lock(mtx);
+            stopThread = true;
+        }
+        cv.notify_one();
+        follower.join();
+    }
+}
+
+void EnvelopeFollower::register
+
 float EnvelopeFollower::Process(float sample){
 
     // Full-wave recitifier
@@ -23,3 +44,11 @@ float EnvelopeFollower::Process(float sample){
 
     return out;
 }
+
+uthread = new std::thread(EnvelopeFollower::exec, this);
+
+static void EnvelopeFollower::exec(EnvelopeFollower* cppThread){
+    cppThread->Process();
+}
+
+//New idea: fly-wheel
